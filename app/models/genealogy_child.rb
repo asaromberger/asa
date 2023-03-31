f4a058881600b040313fa61be9cc696045d69e98
Asa Romberger <asa.romberger@gmail.com>
2023-03-31 16:56:03 -0700

    ASA-62-GenChildrenModel new file:   models/genealogy_child.rb new file:   ../db/migrate/20230331235525_create_genealogy_children.rb modified:   ../db/schema.rb new file:   ../test/fixtures/genealogy_children.yml new file:   ../test/models/genealogy_child_test.rb

---
+++ app/models/genealogy_child.rb
@@ -0,0 +1,2 @@
+class GenealogyChild < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_190336) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_235525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_190336) do
     t.date "date"
     t.integer "bridge_player_id"
     t.float "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genealogy_children", force: :cascade do |t|
+    t.integer "individual_id"
+    t.integer "family_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
