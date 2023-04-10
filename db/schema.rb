1ca0d1af12992880e383635514963088d1ed8f69
Asa Romberger <asa.romberger@gmail.com>
2023-04-10 14:41:43 -0700

    ASA-115-FinanceAccountsModel new file:   models/finance_account.rb new file:   ../db/migrate/20230410214008_create_finance_accounts.rb modified:   ../db/schema.rb new file:   ../test/fixtures/finance_accounts.yml new file:   ../test/models/finance_account_test.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_210336) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_214008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_210336) do
   create_table "finance_accountmaps", force: :cascade do |t|
     t.string "account"
     t.string "ctype"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "finance_accounts", force: :cascade do |t|
+    t.string "account"
+    t.string "atype"
+    t.boolean "closed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
