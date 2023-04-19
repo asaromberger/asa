9554dd3419449ad44d7233e8d2744ae0f786f486
Asa Romberger <asa.romberger@gmail.com>
2023-04-19 16:07:49 -0700

    ASA-158-FinanceExpensesTaxesController new file:   controllers/finance/expenses/taxes_controller.rb new file:   views/finance/expenses/taxes/index.html.erb new file:   views/finance/expenses/taxes/show.html.erb modified:   views/layouts/_menu_finance_expenses.html.erb modified:   ../config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -69,6 +69,7 @@ Rails.application.routes.draw do
 			resources :rent
 			resources :rentalcosts
 			resources :runningbudget
+			resources :taxes
 			resources :yearbudget
 			resources :what_maps
 			resources :whats
